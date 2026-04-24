6eec739c50a2caad981db20833dcc05460ae5edb
~Ajose~ <antonioemprego2000@gmail>
2026-04-24 17:05:06 -0300

    Build bem-sucedido após remoção do vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Verificador de Resiliência Quântica (IRQ)',
-        short_name: 'IRQ Brasil',
-        description: 'Avalie sua preparação para a era quântica.',
-        theme_color: '#0a2b3e',
-        background_color: '#f4f7fc',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,jpg,svg}']
-      }
-    })
-  ]
+  plugins: [react()]
 })
